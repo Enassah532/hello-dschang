2934c21dec01d4cfbf48ff7d60512defd9cc930b
kurohige <kurohige@kali.localhost>
2021-07-05 13:06:28 +0100

    Remove duplicationn by using app layout

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,21 +1,10 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('base')
 
-        <title>About Us | Hello City</title>
-
-    </head>
-
-    <body>
+@section('content')
         
-        <p>Built with &hearts by KUROHIGE.</p>
+    <p>Built with &hearts by KUROHIGE.</p>
 
-        <p><a href="/">Revenir a la page d'acceuille.</a></p>
+    <p><a href="/">Revenir a la page d'acceuille.</a></p>
 
-        <footer>
-            <p>&copy; Copyrigth {{ date('Y') }} </p>
-        </footer>
-    </body>
-</html>
+@endsection
+
