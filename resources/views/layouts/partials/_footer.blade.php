e5015b8c223cf64f1d5e93b130ba663cccee0714
Enassah532 <hmoustaphousmane@gmail.com>
2021-07-11 22:16:15 +0100

    Add email adress to the footer.

--- resources/views/layouts/partials/_footer.blade.php
+++ resources/views/layouts/partials/_footer.blade.php
@@ -1,8 +1,9 @@
 <footer class="text-gray-400">
 <p>
+    <a href="mailto:hmoustaphousmane@gmail.com" class="text-indigo-500 hover:text-indigo-700">Click here to send us an email</a><br>
     &copy; Copyrigth {{ date('Y') }} 
-    @if(! Route::is('app_about'))
-    &middot; <a href="{{ route('app_about') }}" class="text-indigo-500 hover:text-indigo-700 underline">About Us</a>
+    @if(! Route::is('app_about'))&middot;
+    <a href="{{ route('app_about') }}" class="text-indigo-500 hover:text-indigo-700 underline">About Us</a>
     @endif
 </p>
 </footer>
