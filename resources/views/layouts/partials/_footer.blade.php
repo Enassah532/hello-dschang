7c91dbb00935c9f75270b6fc001ae9f1cedeb7c9
Enassah532 <hmoustaphousmane@gmail.com>
2021-07-11 22:46:10 +0100

    Modify email adress.

--- resources/views/layouts/partials/_footer.blade.php
+++ resources/views/layouts/partials/_footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="text-gray-400">
 <p>
-    <a href="mailto:hmoustaphousmane@gmail.com" class="text-indigo-500 hover:text-indigo-700">Click here to send us an email</a><br>
+    <a href="mailto:hmoustaphaousmane@gmail.com" class="text-indigo-500 hover:text-indigo-700">Click here to send us an email</a><br>
     &copy; Copyrigth {{ date('Y') }} 
     @if(! Route::is('app_about'))&middot;
     <a href="{{ route('app_about') }}" class="text-indigo-500 hover:text-indigo-700 underline">About Us</a>
